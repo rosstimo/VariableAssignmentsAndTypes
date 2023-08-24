598393c09469cd7c2259ea3c00da04a10547bc10
tim <rosstimo@isu.edu>
2023-08-24 08:55:05 -0600

    add new example

--- VariableAssignmentsAndTypes/VariableAssignmentsAndTypes.vb
+++ VariableAssignmentsAndTypes/VariableAssignmentsAndTypes.vb
@@ -1,6 +1,16 @@
-﻿Module VariableAssignmentsAndTypes
+﻿Option Explicit On
+Option Strict On
+
+Module VariableAssignmentsAndTypes
 
     Sub Main()
+        Dim fruit As String
+        Dim number As Integer
+        Dim price As Double
+        fruit = "apple"
+        number = 5.976876
+        price = 1.99
+        Console.WriteLine("I have " & number & " " & fruit & "s")
 
     End Sub
 
