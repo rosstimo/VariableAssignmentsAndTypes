7ee2d895aaac67a9e7ff43a31adb3babc034c2b8
tim <rosstimo@isu.edu>
2023-08-28 08:56:16 -0600

    add console intereaction

--- VariableAssignmentsAndTypes/VariableAssignmentsAndTypes.vb
+++ VariableAssignmentsAndTypes/VariableAssignmentsAndTypes.vb
@@ -7,11 +7,25 @@ Module VariableAssignmentsAndTypes
         Dim fruit As String
         Dim number As Integer
         Dim price As Double
+        Dim userInput As String
+        Dim userNumber As Integer
         fruit = "apple"
-        number = 5.976876
+        number = 5
+        'number = 5.976876
         price = 1.99
-        Console.WriteLine("I have " & number & " " & fruit & "s")
+        Console.WriteLine("I have " & CStr(number) & " " & fruit & "s")
+        userInput = Console.ReadLine()
+        Console.WriteLine("You typed: " & userInput)
+        Console.WriteLine($"You typed: {userInput}")
 
+        Console.WriteLine("Please enter a number:")
+        userInput = Console.ReadLine
+        userNumber = CInt(userInput)
+        Console.WriteLine($"You entered: {userNumber}")
+        'add userNumber to number then display the equation and result
+        Console.WriteLine($"{userNumber} + {number} = {userNumber + number}")
+        'pause here until user presses enter
+        Console.Read()
     End Sub
 
 End Module
