78de29cc139361d17e9f03a60ad50edc4e84f854
tim <rosstimo@isu.edu>
2023-08-29 08:24:29 -0600

    add const example

--- VariableAssignmentsAndTypes/VariableAssignmentsAndTypes.vb
+++ VariableAssignmentsAndTypes/VariableAssignmentsAndTypes.vb
@@ -2,13 +2,14 @@
 Option Strict On
 
 Module VariableAssignmentsAndTypes
-
     Sub Main()
         Dim fruit As String
         Dim number As Integer
         Dim price As Double
         Dim userInput As String
         Dim userNumber As Integer
+        Const TAXRATE As Double = 0.06
+        Const PI As Double = 3.1415926535897931
         fruit = "apple"
         number = 5
         'number = 5.976876
